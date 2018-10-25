faae68388635523b18d079556882c2602ea95a16
Nate Clark <natec425@gmail.com>
2018-10-25 14:50:50 -0500

    Add initial draft of backend

---
+++ app.py
@@ -0,0 +1,32 @@
+import hug
+import records
+
+db = records.Database('postgres:///rainfall')
+
+app = hug.http(response_headers={
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+})
+
+
+class Database:
+    @staticmethod
+    def high_score():
+        return db.query('SELECT name, number FROM scores ORDER BY number DESC LIMIT 1').first()
+
+    @staticmethod
+    def new_score(name, number):
+        return db.query('INSERT INTO scores (name, number) VALUES (:name, :number);',
+        		name=name, number=number)
+
+@app.get('/high-score')
+def high_score():
+    high_score_from_db = Database.high_score()
+    return {
+        'name': high_score_from_db.name,
+        'number': high_score_from_db.number,
+    }
+
+@app.post('/new-score')
+def new_score(name: str, number: int):
+    Database.new_score(name=name, number=number)
