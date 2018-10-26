bfcf6bee0fb00d3174642cfff4f868bf753af6d9
ColeAnderson7278 <canderson@basecampcodingacademy.org>
2018-10-26 11:23:33 -0500

    Adds Initial Migration Script
    This adds in a script tag for initializing the database. This
    script creates a scores table to contain names, and the numbers
    corresponding to each name. This script also allows the same
    setup no matter on what machine, or how the database looks
    currently.

---
+++ migrate.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS scores (
+    id SERIAL NOT NULL PRIMARY KEY,
+    name TEXT NOT NULL,
+    number INTEGER NOT NULL
+);
+
+DO $$
+BEGIN
+IF NOT EXISTS (SELECT * FROM scores) THEN
+    INSERT INTO scores (name, number) VALUES
+    ('Henry', 42000);
+END IF;
+END$$;
+
+
