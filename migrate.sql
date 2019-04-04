781e73d2b2e4e14843c7b2399bb42de341d417f4
ColeAnderson7278 <canderson@basecampcodingacademy.org>
2019-04-04 15:24:54 -0500

    Checking for errors.

--- migrate.sql
+++ migrate.sql
@@ -4,3 +4,12 @@ CREATE TABLE IF NOT EXISTS scores (
     number INTEGER NOT NULL
 );
 
+DO $$
+BEGIN
+IF NOT EXISTS (SELECT * FROM scores) THEN
+    INSERT INTO scores (name, number) VALUES
+    ('Me', 42000);
+END IF;
+END$$;
+
+
